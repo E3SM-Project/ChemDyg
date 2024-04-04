a09d3352d3a33dc4eb55cf74c361d04b44b093a6
Hsiang-He Lee <ac.lee1061@chrlogin2.lcrc.anl.gov>
2024-04-04 13:22:31 -0500

    Extend the summary table to the N/S hemisphere

--- chemdyg/templates/chemdyg_summary_table.bash
+++ chemdyg/templates/chemdyg_summary_table.bash
@@ -126,7 +126,7 @@ variablelist = list(h0_in.keys())
 timeperiod = len(h0_in['time'])
 startdate = str(np.array(h0_in['time'].dt.year[0]))+'-01-01'
 
-time_range_month = pd.date_range(startdate,  periods=timeperiod, freq='M')
+time_range_month = pd.date_range(startdate,  periods=timeperiod, freq='ME')
 h0_in['time'] = time_range_month
 h1_in['time'] = time_range_month
 
@@ -135,17 +135,22 @@ unit_covet = 1.e-9*12 # kg/month -> Tg/year
 
 area_rad = h0_in['area'][0]         # radian (ncol)
 area = area_rad * rearth * rearth  # m2
+lat = h0_in['lat'][0]
+NH = area.where(lat >= 0)
+SH = area.where(lat < 0)
+
 lev = h0_in['lev']
 time = h0_in['time']
 
 year = np.array(time.dt.year)
 month = np.array(time.dt.month)
 linehead = '<h> E3SM main chemistry summary (units: Tg/year)</h>'
+linehead = linehead + '<pre>The * sign indicates that the calculation involves the stratosphere, while calculations without the sign primarily pertain to the troposhere. </pre>'
 linehead = linehead + '<pre>'+short_name+'</pre>'
 linehead = linehead + '<pre>Simulation period: '+ startyear +' - '+ endyear + '</pre>'
 line_ann = linehead + '<p> Season: ANN </p>'
 
-line_ann = line_ann + '<pre> Chemistry </pre>'
+line_ann = line_ann + '<pre> Chemistry                          Global        N. Hemisphere   S. Hemisphere </pre>'
 
 fileout_ann = open(pathout+'chem_summary_table.html',"w")
 
@@ -156,6 +161,8 @@ for i in range(len(time)):
 dt_array = xr.DataArray(dt, coords=[h0_in['time']], dims=["time"])
 
 STE_time = np.zeros(len(time))
+STE_time_NH = np.zeros(len(time))
+STE_time_SH = np.zeros(len(time))
 for var in range(len(varname)):
     total_layer = len(layer)
 
@@ -180,6 +187,24 @@ for var in range(len(varname)):
             SCO_total = (SCO*area).sum(axis=1).mean() #kg
             TCO_total = (TCO*area).sum(axis=1).mean() #kg
             TOZ_total = (TOZ*area).sum(axis=1).mean() #kg
+            # NH
+            MSD_NH = ((MSD*NH).sum(axis=1)).mean() #kg
+            TDD_NH = (dt*(TDD*NH).sum(axis=1)).mean() #kg
+            CIP_NH = (dt*(CIP*NH).sum(axis=1)).mean()
+            CIL_NH = (dt*(-CIL*NH).sum(axis=1)).mean()
+            NET_NH = (dt*((CIP-CIL)*NH).sum(axis=1)).mean()
+            SCO_NH = (SCO*NH).sum(axis=1).mean() #kg
+            TCO_NH = (TCO*NH).sum(axis=1).mean() #kg
+            TOZ_NH = (TOZ*NH).sum(axis=1).mean() #kg
+            # SH
+            MSD_SH = ((MSD*SH).sum(axis=1)).mean() #kg
+            TDD_SH = (dt*(TDD*SH).sum(axis=1)).mean() #kg
+            CIP_SH = (dt*(CIP*SH).sum(axis=1)).mean()
+            CIL_SH = (dt*(-CIL*SH).sum(axis=1)).mean()
+            NET_SH = (dt*((CIP-CIL)*SH).sum(axis=1)).mean()
+            SCO_SH = (SCO*SH).sum(axis=1).mean() #kg
+            TCO_SH = (TCO*SH).sum(axis=1).mean() #kg
+            TOZ_SH = (TOZ*SH).sum(axis=1).mean() #kg
             # calculate STE
             for i in range(len(time)):
                 MSDt = h1_in[varname[var]+'_2DMSD_trop'][i,:] #kg/m2
@@ -203,33 +228,57 @@ for var in range(len(varname)):
 
                 if i == 0:
                     STE = 'nan'
+                    STE_NH = 'nan'
+                    STE_SH = 'nan'
                 else:
                     temp = MSD_old+td_temp
                     STE = ((MSDt-temp)*area).sum()
+                    STE_NH = ((MSDt-temp)*NH).sum()
+                    STE_SH = ((MSDt-temp)*SH).sum()
 
                 STE_time[i] = STE
+                STE_time_NH[i] = STE_NH
+                STE_time_SH[i] = STE_SH
 
                 MSD_old = MSDt
 
             STE_mean = STE_time[1::].mean()
+            STE_mean_NH = STE_time_NH[1::].mean()
+            STE_mean_SH = STE_time_SH[1::].mean()
 
    # write out annual chem tendency
             line_ann = line_ann + '<pre> '+ format('O3 burden (Tg)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TOZ_total)) +'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TOZ_total))
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TOZ_NH))
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TOZ_SH)) +'</pre>'
             line_ann = line_ann + '<pre> '+ format('O3 deposition (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet) +'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_NH)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_SH)*unit_covet) +'</pre>'
             line_ann = line_ann + '<pre> '+ format('O3 production (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CIP_total)*unit_covet) +'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CIP_total)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CIP_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CIP_SH)*unit_covet) +'</pre>'
             line_ann = line_ann + '<pre> '+ format('O3 loss (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CIL_total)*unit_covet) +'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CIL_total)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CIL_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CIL_SH)*unit_covet) +'</pre>'
             line_ann = line_ann + '<pre> '+ format('O3 net change (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(NET)*unit_covet) +'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(NET)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(NET_NH)*unit_covet) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(NET_SH)*unit_covet) +'</pre>'
             line_ann = line_ann + '<pre> '+ format('O3 TCO (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TCO_total)) +'</pre>'
-            line_ann = line_ann + '<pre> '+ format('O3 SCO (Tg)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(SCO_total)) +'</pre>'
-            line_ann = line_ann + '<pre> '+ format('O3 STE (Tg)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(STE_mean)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TCO_total)) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TCO_NH))
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TCO_SH)) +'</pre>'
+            line_ann = line_ann + '<pre> '+ format('O3 SCO (Tg)*','28s')
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(SCO_total)) 
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(SCO_NH))
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(SCO_SH)) +'</pre>'
+            line_ann = line_ann + '<pre> '+ format('O3 STE (Tg)*','28s')
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(STE_mean)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(STE_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(STE_SH)*unit_covet)+'</pre>'
 
         elif varname[var] == 'CO':
             MSD = h1_in[varname[var]+'_2DMSD'] #kg/m2
@@ -245,19 +294,45 @@ for var in range(len(varname)):
             CEP_total = (dt*(CEP*area).sum(axis=1)).mean()
             CEL_total = (dt*(CEL*area).sum(axis=1)).mean()
             NET2      = (dt*((CEP+CEL)*area).sum(axis=1)).mean()
+           #NH
+            MSD_NH    = ((MSD*NH).sum(axis=1)).mean() #kg
+            TDS_NH    = (dt*(TDS*NH).sum(axis=1)).mean() #kg
+            TDD_NH    = (dt*(TDD*NH).sum(axis=1)).mean() #kg
+            CEP_NH    = (dt*(CEP*NH).sum(axis=1)).mean()
+            CEL_NH    = (dt*(CEL*NH).sum(axis=1)).mean()
+            NET2_NH   = (dt*((CEP+CEL)*NH).sum(axis=1)).mean()
+           #SH
+            MSD_SH    = ((MSD*SH).sum(axis=1)).mean() #kg
+            TDS_SH    = (dt*(TDS*SH).sum(axis=1)).mean() #kg
+            TDD_SH    = (dt*(TDD*SH).sum(axis=1)).mean() #kg
+            CEP_SH    = (dt*(CEP*SH).sum(axis=1)).mean()
+            CEL_SH    = (dt*(CEL*SH).sum(axis=1)).mean()
+            NET2_SH   = (dt*((CEP+CEL)*SH).sum(axis=1)).mean()
 
             line_ann = line_ann + '<pre> '+ format('CO burden (Tg)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CO emission (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CO deposition (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CO production (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEP_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEP_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEP_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEP_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CO loss (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEL_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEL_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEL_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEL_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CO net change (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(NET2)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(NET2)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(NET2_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(NET2_SH)*unit_covet)+'</pre>'
         elif varname[var] == 'CH4LNZ':
             if varname[var]+'_2DTDS' in variablelist:
                 print(varname[var])
@@ -274,17 +349,35 @@ for var in range(len(varname)):
             TDS_total = (dt*(TDS*area).sum(axis=1)).mean() #kg
             TDD_total = (dt*(TDD*area).sum(axis=1)).mean() #kg
             CEL_total = (dt*(CEL*area).sum(axis=1)).mean()
+           #NH
+            MSD_NH = ((MSD*NH).sum(axis=1)).mean() #kg
+            TDS_NH = (dt*(TDS*NH).sum(axis=1)).mean() #kg
+            TDD_NH = (dt*(TDD*NH).sum(axis=1)).mean() #kg
+            CEL_NH = (dt*(CEL*NH).sum(axis=1)).mean()
+           #SH
+            MSD_SH = ((MSD*SH).sum(axis=1)).mean() #kg
+            TDS_SH = (dt*(TDS*SH).sum(axis=1)).mean() #kg
+            TDD_SH = (dt*(TDD*SH).sum(axis=1)).mean() #kg
+            CEL_SH = (dt*(CEL*SH).sum(axis=1)).mean()
 
             line_ann = line_ann + '<pre> '+ format('CH4 burden (Tg)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CH4 emission (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CH4 deposition (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CH4 production (Tg/yr)','28s')
             line_ann = line_ann + '      --------     '+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CH4 loss (Tg/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEL_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(CEL_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEL_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(CEL_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('CH4 net change (Tg/yr)','28s')
             line_ann = line_ann + '      --------     '+'</pre>'
         elif varname[var] == 'NO':
@@ -298,27 +391,55 @@ for var in range(len(varname)):
             MSD_total = ((MSD*area).sum(axis=1)).mean() #kg
             TDS_total = (dt*(TDS*area).sum(axis=1)).mean() #kg
             TDD_total = (dt*(TDD*area).sum(axis=1)).mean() #kg
+            MSD_NH = ((MSD*NH).sum(axis=1)).mean() #kg
+            TDS_NH = (dt*(TDS*NH).sum(axis=1)).mean() #kg
+            TDD_NH = (dt*(TDD*NH).sum(axis=1)).mean() #kg
+            MSD_SH = ((MSD*SH).sum(axis=1)).mean() #kg
+            TDS_SH = (dt*(TDS*SH).sum(axis=1)).mean() #kg
+            TDD_SH = (dt*(TDD*SH).sum(axis=1)).mean() #kg
             if 'NO_TDLgt' in variablelist:
                 LGT_3d = LGT.copy()
                 ACF_3d = ACF.copy()
+                LGT_NH = LGT.copy()
+                ACF_NH = ACF.copy()
+                LGT_SH = LGT.copy()
+                ACF_SH = ACF.copy()
                 for k in range(len(lev)):
                     LGT_3d[:,k,:] = LGT[:,k,:]*area
                     ACF_3d[:,k,:] = ACF[:,k,:]*area
+                    LGT_NH[:,k,:] = LGT[:,k,:]*NH
+                    ACF_NH[:,k,:] = ACF[:,k,:]*NH
+                    LGT_SH[:,k,:] = LGT[:,k,:]*SH
+                    ACF_SH[:,k,:] = ACF[:,k,:]*SH
              
                 LGT_total = (dt*LGT_3d.sum(axis=1).sum(axis=1)).mean() 
                 ACF_total = (dt*ACF_3d.sum(axis=1).sum(axis=1)).mean() 
+                LGT_Ntotal = (dt*LGT_NH.sum(axis=1).sum(axis=1)).mean() 
+                ACF_Ntotal = (dt*ACF_NH.sum(axis=1).sum(axis=1)).mean() 
+                LGT_Stotal = (dt*LGT_SH.sum(axis=1).sum(axis=1)).mean() 
+                ACF_Stotal = (dt*ACF_SH.sum(axis=1).sum(axis=1)).mean() 
 
             line_ann = line_ann + '<pre> '+ format('NOx burden (Tg N)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(MSD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(MSD_SH)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('NOx emission (Tg N/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDS_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDS_SH)*unit_covet)+'</pre>'
             if 'NO_TDLgt' in variablelist:
                 line_ann = line_ann + '<pre> '+ format('NOx lightning emis (Tg N/yr)','28s')
-                line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(LGT_total)*unit_covet)+'</pre>'
+                line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(LGT_total)*unit_covet)
+                line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(LGT_Ntotal)*unit_covet)
+                line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(LGT_Stotal)*unit_covet)+'</pre>'
                 line_ann = line_ann + '<pre> '+ format('NOx Aircraft emis (Tg N/yr)','28s')
-                line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(ACF_total)*unit_covet)+'</pre>'
+                line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(ACF_total)*unit_covet)
+                line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(ACF_Ntotal)*unit_covet)
+                line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(ACF_Stotal)*unit_covet)+'</pre>'
             line_ann = line_ann + '<pre> '+ format('NOx deposition (Tg N/yr)','28s')
-            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)+'</pre>'
+            line_ann = line_ann + '     '+"{0:+.3e}".format(np.array(TDD_total)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_NH)*unit_covet)
+            line_ann = line_ann + '       '+"{0:+.3e}".format(np.array(TDD_SH)*unit_covet)+'</pre>'
 
 fileout_ann.write(line_ann)
 fileout_ann.close()
